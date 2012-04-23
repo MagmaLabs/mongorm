f7afd4a2cd96dbc1952c88f6fff9d551c01abb58
Dougall Johnson <dougallj@gmail.com>
2012-04-23 11:28:24 +1000

    Allow None in DateTimeField

--- mongorm/fields/DateTimeField.py
+++ mongorm/fields/DateTimeField.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 class DateTimeField(BaseField):
 	def fromPython( self, pythonValue ):
-		if not isinstance(pythonValue, datetime):
-			raise ValueError, "Value must be a datetime object"
+		if pythonValue is not None and not isinstance(pythonValue, datetime):
+			raise ValueError, "Value must be a datetime object not %r" % (pythonValue,)
+			
 		return pythonValue
 	
 	def toPython( self, bsonValue ):
